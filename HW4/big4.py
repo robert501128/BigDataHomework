13d5967b708f3e27224c65c3ab5b5e226812ffab
robert501128 <robert501128@gmail.com>
2016-06-14 00:12:03 +0800

    add pre-processing file

---
+++ HW4/big4.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+from pyspark.sql.functions import udf
+
+numFile = 3
+sqlContext = SQLContext(sc)
+dflist = []
+column_udf = udf(lambda name: 0 if name == 'NA' else int(name), IntegerType())
+for i in range(numFile):
+  x = sqlContext.read.format('com.databricks.spark.csv').options(header='true',inferschema='true').load('flight/'+str(1987+i)+'.csv')
+  x = x.select('Year','Month','DayofMonth','DayOfWeek','DepTime','CRSDepTime','ArrTime','CRSArrTime','UniqueCarrier'\
+                            ,'FlightNum','TailNum','ActualElapsedTime','CRSElapsedTime','AirTime','ArrDelay','DepDelay', 'Origin'\
+                            ,'Dest','Distance','TaxiIn','TaxiOut','Cancelled')
+  xtype = x.dtypes
+   for t in xtype:
+     if t[1]=='string' and t[0] not in ['Origin','Dest','TailNum','UniqueCarrier','FlightNum']
+       x=x.withColumn(t[0],new_column_udf(x[t[0]]))
+  x.write.parquet('flight/dataset/key='+str(i))
+  dflist.append(x)
