c9636c0bc9e86508058c9d9a8e37879c6e0a9ac5
robert501128 <robert501128@gmail.com>
2016-06-14 01:23:15 +0800

    yeah

--- HW4/big4.py
+++ HW4/big4.py
@@ -7,13 +7,14 @@ sqlContext = SQLContext(sc)
 dflist = []
 column_udf = udf(lambda name: 0 if name == 'NA' else int(name), IntegerType())
 for i in range(numFile):
-  x = sqlContext.read.format('com.databricks.spark.csv').options(header='true',inferschema='true').load('flight/'+str(1987+i)+'.csv')
+  x = sqlContext.read.format('com.databricks.spark.csv').options(header='true',inferschema='true',nullValue='NA').load('flight/'+str(1987+i)+'.csv')
   x = x.select('Year','Month','DayofMonth','DayOfWeek','DepTime','CRSDepTime','ArrTime','CRSArrTime','UniqueCarrier'\
                             ,'FlightNum','TailNum','ActualElapsedTime','CRSElapsedTime','AirTime','ArrDelay','DepDelay', 'Origin'\
                             ,'Dest','Distance','TaxiIn','TaxiOut','Cancelled')
+  x=x.na.fill('0')
   xtype = x.dtypes
-   for t in xtype:
-     if t[1]=='string' and t[0] not in ['Origin','Dest','TailNum','UniqueCarrier','FlightNum']
-       x=x.withColumn(t[0],new_column_udf(x[t[0]]))
+  for t in xtype:
+     if t[1]=='string' and t[0] not in ['Origin','Dest','TailNum','UniqueCarrier','FlightNum']:
+       x=x.withColumn(t[0],x[t[0]].cast('integer'))       
   x.write.parquet('flight/dataset/key='+str(i))
   dflist.append(x)
